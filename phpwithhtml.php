c8afd01496b9ded44da939e4c018adf4912a68c5
mdirgha18 <dirgha.mankad702@gmail.com>
2026-03-07 21:23:26 +0530

    Add PHP with HTML example file

---
+++ phpwithhtml.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP with HTML Example</title>
+</head>
+<body>
+    <h1>Welcome to my Website</h1>
+    <?php
+    $name = "John";
+    echo "<p>Hello, $name!</p>";
+
+?>
+
+<p>This is a paragraph written in HTML</p>
+</body>
+</html>
