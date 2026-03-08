ef95b9df2ce04b7974808d97b74c9348cda28104
mdirgha18 <dirgha.mankad702@gmail.com>
2026-03-08 21:19:48 +0530

    Add isarray.php to check variable types

---
+++ isarray.php
@@ -0,0 +1,20 @@
+<!-- <?php
+$variable_name1 = 67.099;
+$variable_name2=32;
+$variable_name3="abc";
+$variable_name4=array('A','B','C');
+if(is_array($variable_name1))
+    echo "array('A', 'B', 'C') is an array. \n";
+else
+    echo "array('A', 'B', 'C') is not a array value. \n";
+if(is_array($variable_name2))
+    echo "$variable_name2 is a array value. \n";
+else
+    echo "$variable_name2 is not a array value. \n";
+
+if(is_array($variable_name3))
+    echo "$variable_name3 is a array value. \n";
+else
+    echo "$variable_name3 is not a array value. \n";
+?> -->
+
