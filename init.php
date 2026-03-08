05117e4978506d14d78b540f63bdf24e86ab6176
mdirgha18 <dirgha.mankad702@gmail.com>
2026-03-08 06:11:28 +0530

    Add init.php with integer checks for variables

---
+++ init.php
@@ -0,0 +1,22 @@
+<?php
+$variable_name1 = 56;
+$variable_name2 = "xyz";
+
+if(is_int($variable_name1))
+{
+    echo "$variable_name1 is integer \n";
+}
+else
+{
+    echo "$variable_name1 is not an integer \n";
+}
+
+if (is_int($variable_name2))
+{
+    echo "$variable_name2 is interger \n";
+}
+else
+{
+    echo "$variable_name2 is not integer \n";
+}
+?>
